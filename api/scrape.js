fdff04f5d3b8089f330735b08ef579e98b60f239
jrichardw <40728300+jrichardw@users.noreply.github.com>
2024-02-21 09:04:50 -0500

    added puppeteer core, dynamic env logic, error handling

--- api/scrape.js
+++ api/scrape.js
@@ -1,5 +1,8 @@
 // api/scrape.js
-const puppeteer = require('puppeteer');
+const chromium = require('@sparticuz/chromium');
+// Dynamically require the appropriate Puppeteer package
+const puppeteer = process.env.NODE_ENV === 'production' ? require('puppeteer-core') : require('puppeteer');
+
 
 module.exports = async (req, res) => {
     const url = req.query.url; // Get the URL to scrape from the query parameters
@@ -9,11 +12,25 @@ module.exports = async (req, res) => {
         return;
     }
 
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle0' }); // wait until page load
-    const content = await page.content(); // get page content
-    await browser.close();
-
-    res.status(200).send(content);
+    const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    let browser = null;
+    try {
+        const launchOptions = process.env.NODE_ENV === 'production' ? {
+                args: chromium.args,
+                executablePath: await chromium.executablePath(),
+                headless: true,
+            } : {};
+    
+        browser = await puppeteer.launch(launchOptions);
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'networkidle0' }); // wait until page load
+        const content = await page.content(); // get page content
+        await browser.close();
+    
+        res.status(200).send(content);
+    } catch (error) {
+        console.error("An error occurred during data extraction", error);
+        if (browser !== null) await browser.close();
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
 };
